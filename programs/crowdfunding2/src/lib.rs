e2e23734134e518491cb13c4e959f6498b08316c
chenrun666 <7417245+chenrun666@user.noreply.gitee.com>
2025-10-19 11:20:45 +0800

    feat: add create, withdraw, donate

--- programs/crowdfunding2/src/lib.rs
+++ programs/crowdfunding2/src/lib.rs
@@ -1,3 +1,4 @@
+use anchor_lang::prelude::entrypoint::ProgramResult;
 use anchor_lang::prelude::*;
 
 declare_id!("BrpDKtbu9Z6dHZteU1sQc8644QDZCgjNKU1KUNiGhrQQ");
@@ -6,11 +7,91 @@ declare_id!("BrpDKtbu9Z6dHZteU1sQc8644QDZCgjNKU1KUNiGhrQQ");
 pub mod crowdfunding2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.target_amount = 0;
+        campaign.manager = ctx.accounts.manager.key();
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<WithDraw>, amount: u64) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        if ctx.accounts.manager.key() != campaign.manager {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **ctx
+            .accounts
+            .manager
+            .to_account_info()
+            .try_borrow_mut_lamports()? += amount;
+        Ok(())
+    }
+
+    pub fn donate(ctx: Context<WithDraw>, amount: u64) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        // **ctx
+        //     .accounts
+        //     .manager
+        //     .to_account_info()
+        //     .try_borrow_mut_lamports()? -= amount;
+        // **campaign.to_account_info().try_borrow_mut_lamports()? += amount;
+        // campaign.target_amount += amount;
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.manager.key(),
+            &campaign.key(),
+            amount,
+        );
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.manager.to_account_info(),
+                campaign.to_account_info(),
+            ],
+        )?;
+        campaign.target_amount += amount;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = manager, space = 8 + Campaign::INIT_SPACE)]
+    pub campaign: Account<'info, Campaign>,
+
+    #[account(mut)]
+    pub manager: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WithDraw<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+
+    #[account(mut)]
+    pub manager: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Donate<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+
+    #[account(mut)]
+    pub manager: Signer<'info>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Campaign {
+    #[max_len(100)]
+    pub name: String,
+    #[max_len(100)]
+    pub description: String,
+    pub target_amount: u64,
+    pub manager: Pubkey,
+}
--- tests/crowdfunding2.ts
+++ tests/crowdfunding2.ts
@@ -1,16 +1,35 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Crowdfunding2 } from "../target/types/crowdfunding2";
+import { Keypair } from "@solana/web3.js";
+import { assert } from "chai";
 
 describe("crowdfunding2", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider;
+
+  anchor.setProvider(provider.env());
 
   const program = anchor.workspace.crowdfunding2 as Program<Crowdfunding2>;
 
-  it("Is initialized!", async () => {
+  const payer = Keypair.generate();
+
+  it("create", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .create("Hello", "World")
+      .accounts({
+        campaign: payer.publicKey,
+      })
+      .signers([payer])
+      .rpc();
     console.log("Your transaction signature", tx);
+    const campaignAccount = await program.account.campaign.fetch(
+      payer.publicKey
+    );
+    console.log("campaign name:", campaignAccount.name);
+    console.log("campaign description:", campaignAccount.description);
+    assert.ok(campaignAccount.name, "Hello");
+    assert.ok(campaignAccount.description, "World");
   });
 });
