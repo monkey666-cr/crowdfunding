a16e5ed4131f1f334c1320e21620c6ad6a8e6319
chenrun666 <7417245+chenrun666@user.noreply.gitee.com>
2025-10-19 11:29:47 +0800

    init: initialize frontend

---
+++ frontend/src/app/solana/page.tsx
@@ -0,0 +1,8 @@
+export default function SolanaPage() {
+  return (
+    <div>
+      <h1>Solana Crowdfunding DApp</h1>
+      <p>Welcome to the Solana Crowdfunding decentralized application!</p>
+    </div>
+  );
+}
